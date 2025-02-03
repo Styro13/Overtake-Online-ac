eee9864a0b238603c978a9d4795cbbcca3acbdc3
Jose E. Vega <73301128+Styro13@users.noreply.github.com>
2025-02-03 10:17:12 -0500

    Translation
    No real changes. Code comments translated to english.

--- Atilxor-overtake.lua
+++ Atilxor-overtake.lua
@@ -1,18 +1,21 @@
--- Author: Atilxor
--- Version: 0.5
+-- Comments Translated
 
+
+-- VERY IMPORTANT TO REMEMBER! **
+-- Author: Atilxor
+-- VERY IMPORTANT TO REMEMBER! **
 
 local requiredSpeed = 95
 
--- تابع پیش‌زمینه (prepare) برای اجرا پیش از شروع اصلی
+-- prepare function to execute before the main start
 function script.prepare(dt)
-    -- نمایش سرعت ماشین بازیکن در کنسول اختصاصی
+    -- Display the player's car speed in the dedicated console
     ac.debug("speed", ac.getCarState(1).speedKmh)
-    -- بررسی شرط برای ادامه اجرا
+    -- Check the condition to continue execution
     return ac.getCarState(1).speedKmh > 60
 end
 
--- وضعیت رویداد:
+-- Event status:
 local timePassed = 0
 local totalScore = 0
 local comboMeter = 1
@@ -22,53 +25,53 @@ local dangerouslySlowTimer = 0
 local carsState = {}
 local wheelsWarningTimeout = 0
 
--- تابع بروزرسانی (update) برای اجرا در هر فریم
+-- update function to execute at each frame
 function script.update(dt)
-    -- اگر زمان گذشته برابر صفر باشد، پیام "شروع!" نمایش داده می‌شود
+    -- If the elapsed time is zero, the message "Let's start!" is displayed
     if timePassed == 0 then
-        addMessage("بیایید شروع کنیم!", 0)
+        addMessage("Let's start!", 0)
     end
 
-    -- دریافت وضعیت ماشین بازیکن
+    -- Get the player's car status
     local player = ac.getCarState(1)
 
-    -- اگر میزان دیگرانه‌گری (engineLifeLeft) کمتر از 1 شود
+    -- If engineLifeLeft is less than 1
     if player.engineLifeLeft < 1 then
-        -- اگر مجموع امتیازات بیشتر از بالاترین امتیاز ثبت شده باشد، بالاترین امتیاز به مقدار صحیح محاسبه می‌شود
+        -- If the total score is greater than the highest recorded score, the highest score is calculated correctly
         if totalScore > highestScore then
             highestScore = math.floor(totalScore)
-            -- ارسال پیام چت به بازی با امتیاز به همراه متن
-            ac.sendChatMessage("امتیاز " .. totalScore .. " امتیاز را کسب کرد.")
+            -- Send a chat message to the game with the score and text
+            ac.sendChatMessage("Scored " .. totalScore .. " points.")
         end
-        -- مجموع امتیازات صفر می‌شود و میزان دیگرانه‌گری به حالت اولیه برگشت می‌شود
+        -- The total score is reset to zero, and the engineLifeLeft is restored to the initial state
         totalScore = 0
         comboMeter = 1
         return
     end
 
-    -- افزایش زمان گذشته با مقدار زمان گذشته از آخرین فریم
+    -- Increase elapsed time by the amount of time passed since the last frame
     timePassed = timePassed + dt
 
-    -- محاسبه میزان کاهش کمبو به ازای گذشت زمان و سرعت ماشین
+    -- Calculate the combo decay rate based on elapsed time and car speed
     local comboFadingRate = 0.5 * math.lerp(1, 0.1, math.lerpInvSat(player.speedKmh, 80, 200)) + player.wheelsOutside
     comboMeter = math.max(1, comboMeter - dt * comboFadingRate)
 
-    -- دریافت وضعیت شبیه‌ساز بازی
+    -- Get the game simulator state
     local sim = ac.getSimState()
 
-    -- تعداد ماشین‌ها به تعداد وضعیت ماشین‌ها در آرایه ماشین‌ها افزوده می‌شود
+    -- Increase the number of cars to match the number of car states in the cars array
     while sim.carsCount > #carsState do
         carsState[#carsState + 1] = {}
     end
 
-    -- اگر زمان محدودیت هشدار چرخ‌ها (wheelsWarningTimeout) بیشتر از صفر باشد، کاهش می‌یابد
+    -- If the wheels warning timeout is greater than zero, decrease it
     if wheelsWarningTimeout > 0 then
         wheelsWarningTimeout = wheelsWarningTimeout - dt
     elseif player.wheelsOutside > 0 then
-        -- اگر چرخ‌های ماشین خارج از مسیر باشند، پیام هشدار نمایش داده می‌شود
+        -- If the car's wheels are off the track, display a warning message
         if wheelsWarningTimeout == 0 then
         end
-        addMessage("ماشین خارج از مسیر است", -1)
+        addMessage("The car is off the track", -1)
         wheelsWarningTimeout = 60
     end
 
@@ -76,13 +79,13 @@ function script.update(dt)
         if dangerouslySlowTimer > 3 then
             if totalScore > highestScore then
                 highestScore = math.floor(totalScore)
-                ac.sendChatMessage("scored " .. totalScore .. " points.")
+                ac.sendChatMessage("Scored " .. totalScore .. " points.")
             end
             totalScore = 0
             comboMeter = 1
         else
             if dangerouslySlowTimer == 0 then
-                addMessage("Speed Ro Bishtar Kon!", -1)
+                addMessage("Increase your speed!", -1)
             end
         end
         dangerouslySlowTimer = dangerouslySlowTimer + dt
@@ -120,7 +123,7 @@ function script.update(dt)
 
                 if totalScore > highestScore then
                     highestScore = math.floor(totalScore)
-                    ac.sendChatMessage("scored " .. totalScore .. " points.")
+                    ac.sendChatMessage("Scored " .. totalScore .. " points.")
                 end
                 totalScore = 0
                 comboMeter = 1
@@ -210,80 +213,79 @@ local function updateMessages(dt)
 end
 
 local speedWarning = 0
-    function script.drawUI()
-        local uiState = ac.getUiState()
-        updateMessages(uiState.dt)
+function script.drawUI()
+    local uiState = ac.getUiState()
+    updateMessages(uiState.dt)
 
-        local speedRelative = math.saturate(math.floor(ac.getCarState(1).speedKmh) / requiredSpeed)
-        speedWarning = math.applyLag(speedWarning, speedRelative < 1 and 1 or 0, 0.5, uiState.dt)
+    local speedRelative = math.saturate(math.floor(ac.getCarState(1).speedKmh) / requiredSpeed)
+    speedWarning = math.applyLag(speedWarning, speedRelative < 1 and 1 or 0, 0.5, uiState.dt)
 
-        local colorDark = rgbm(0.4, 0.4, 0.4, 1)
-        local colorGrey = rgbm(0.7, 0.7, 0.7, 1)
-        local colorAccent = rgbm.new(hsv(speedRelative * 120, 1, 1):rgb(), 1)
-        local colorCombo =
-            rgbm.new(hsv(comboColor, math.saturate(comboMeter / 10), 1):rgb(), math.saturate(comboMeter / 4))
+    local colorDark = rgbm(0.4, 0.4, 0.4, 1)
+    local colorGrey = rgbm(0.7, 0.7, 0.7, 1)
+    local colorAccent = rgbm.new(hsv(speedRelative * 120, 1, 1):rgb(), 1)
+    local colorCombo = rgbm.new(hsv(comboColor, math.saturate(comboMeter / 10), 1):rgb(), math.saturate(comboMeter / 4))
 
-        local function speedMeter(ref)
-            ui.drawRectFilled(ref + vec2(0, -4), ref + vec2(180, 5), colorDark, 1)
-            ui.drawLine(ref + vec2(0, -4), ref + vec2(0, 4), colorGrey, 1)
-            ui.drawLine(ref + vec2(requiredSpeed, -4), ref + vec2(requiredSpeed, 4), colorGrey, 1)
+    local function speedMeter(ref)
+        ui.drawRectFilled(ref + vec2(0, -4), ref + vec2(180, 5), colorDark, 1)
+        ui.drawLine(ref + vec2(0, -4), ref + vec2(0, 4), colorGrey, 1)
+        ui.drawLine(ref + vec2(requiredSpeed, -4), ref + vec2(requiredSpeed, 4), colorGrey, 1)
 
-            local speed = math.min(ac.getCarState(1).speedKmh, 180)
-            if speed > 1 then
-                ui.drawLine(ref + vec2(0, 0), ref + vec2(speed, 0), colorAccent, 4)
-            end
+        local speed = math.min(ac.getCarState(1).speedKmh, 180)
+        if speed > 1 then
+            ui.drawLine(ref + vec2(0, 0), ref + vec2(speed, 0), colorAccent, 4)
         end
-
-        ui.beginTransparentWindow("overtakeScore", vec2(100, 100), vec2(400 * 0.5, 400 * 0.5))
-        ui.beginOutline()
-
-        ui.pushStyleVar(ui.StyleVar.Alpha, 1 - speedWarning)
-        ui.pushFont(ui.Font.Main)
-        ui.text('Made By Atilxor\'')
-        ui.text("Highest Score: " .. highestScore .. " pts")
-        ui.popFont()
-        ui.popStyleVar()
-
-        ui.pushFont(ui.Font.Title)
-        ui.text(totalScore .. " pts")
-        ui.sameLine(0, 20)
-        ui.beginRotation()
-        ui.textColored(math.ceil(comboMeter * 10) / 10 .. "x", colorCombo)
-        if comboMeter > 20 then
-            ui.endRotation(math.sin(comboMeter / 180 * 3141.5) * 3 * math.lerpInvSat(comboMeter, 20, 30) + 90)
-        end
-        ui.popFont()
-        ui.endOutline(rgbm(0, 0, 0, 0.3))
-
-        ui.offsetCursorY(20)
-        ui.pushFont(ui.Font.Main)
-        local startPos = ui.getCursor()
-        for i = 1, #messages do
-            local m = messages[i]
-            local f = math.saturate(4 - m.currentPos) * math.saturate(8 - m.age)
-            ui.setCursor(startPos + vec2(20 * 0.5 + math.saturate(1 - m.age * 10) ^ 2 * 50, (m.currentPos - 1) * 15))
-            ui.textColored(
-                m.text,
-                m.mood == 1 and rgbm(0, 1, 0, f) or m.mood == -1 and rgbm(1, 0, 0, f) or rgbm(1, 1, 1, f)
-            )
-        end
-        for i = 1, glitterCount do
-            local g = glitter[i]
-            if g ~= nil then
-                ui.drawLine(g.pos, g.pos + g.velocity * 4, g.color, 2)
-            end
-        end
-        ui.popFont()
-        ui.setCursor(startPos + vec2(0, 4 * 30))
-
-        ui.pushStyleVar(ui.StyleVar.Alpha, speedWarning)
-        ui.setCursorY(0)
-        ui.pushFont(ui.Font.Main)
-        ui.textColored("Keep speed above " .. requiredSpeed .. " km/h:", colorAccent)
-        speedMeter(ui.getCursor() + vec2(-9 * 0.5, 4 * 0.2))
-
-        ui.popFont()
-        ui.popStyleVar()
-
-        ui.endTransparentWindow()
     end
+
+    ui.beginTransparentWindow("overtakeScore", vec2(100, 100), vec2(400 * 0.5, 400 * 0.5))
+    ui.beginOutline()
+
+    ui.pushStyleVar(ui.StyleVar.Alpha, 1 - speedWarning)
+    ui.pushFont(ui.Font.Main)
+    ui.text('Made By Atilxor\'')
+    ui.text("Highest Score: " .. highestScore .. " pts")
+    ui.popFont()
+    ui.popStyleVar()
+
+    ui.pushFont(ui.Font.Title)
+    ui.text(totalScore .. " pts")
+    ui.sameLine(0, 20)
+    ui.beginRotation()
+    ui.textColored(math.ceil(comboMeter * 10) / 10 .. "x", colorCombo)
+    if comboMeter > 20 then
+        ui.endRotation(math.sin(comboMeter / 180 * 3141.5) * 3 * math.lerpInvSat(comboMeter, 20, 30) + 90)
+    end
+    ui.popFont()
+    ui.endOutline(rgbm(0, 0, 0, 0.3))
+
+    ui.offsetCursorY(20)
+    ui.pushFont(ui.Font.Main)
+    local startPos = ui.getCursor()
+    for i = 1, #messages do
+        local m = messages[i]
+        local f = math.saturate(4 - m.currentPos) * math.saturate(8 - m.age)
+        ui.setCursor(startPos + vec2(20 * 0.5 + math.saturate(1 - m.age * 10) ^ 2 * 50, (m.currentPos - 1) * 15))
+        ui.textColored(
+            m.text,
+            m.mood == 1 and rgbm(0, 1, 0, f) or m.mood == -1 and rgbm(1, 0, 0, f) or rgbm(1, 1, 1, f)
+        )
+    end
+    for i = 1, glitterCount do
+        local g = glitter[i]
+        if g ~= nil then
+            ui.drawLine(g.pos, g.pos + g.velocity * 4, g.color, 2)
+        end
+    end
+    ui.popFont()
+    ui.setCursor(startPos + vec2(0, 4 * 30))
+
+    ui.pushStyleVar(ui.StyleVar.Alpha, speedWarning)
+    ui.setCursorY(0)
+    ui.pushFont(ui.Font.Main)
+    ui.textColored("Keep speed above " .. requiredSpeed .. " km/h:", colorAccent)
+    speedMeter(ui.getCursor() + vec2(-9 * 0.5, 4 * 0.2))
+
+    ui.popFont()
+    ui.popStyleVar()
+
+    ui.endTransparentWindow()
+end
